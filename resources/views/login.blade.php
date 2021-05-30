4a3811319db06a32856388d72231678af9c56064
Marcellin DJOUBISSIE <Marcellin.Djoubissie@cas-assistance.local>
2021-05-30 09:49:07 +0100

    login done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,16 +5,17 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 col-sm-offset-4">
-            <form>  
+            <form action="login" method="POST">  
+                @csrf
                 <div class="form-group"> 
                     <label for="email">User Name</label> 
-                    <input type="email" class="form-control" id="email" placeholder="Enter email"> 
+                    <input type="email" name="email" class="form-control" id="email" placeholder="Enter email"> 
                 </div>  
                 <div class="form-group"> 
                     <label for="pwd">Password</label> 
-                    <input type="password" class="form-control" id="pwd" placeholder="Enter password"> 
+                    <input type="password" name="password" class="form-control" id="pwd" placeholder="Enter password"> 
                 </div>  
-                <button type="submit" class="btn btn-primary">Submit</button> 
+                <button type="submit" class="btn btn-primary">Login</button> 
             </form> 
         </div>
     </div>
